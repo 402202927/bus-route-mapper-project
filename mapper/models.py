fba6c455ed33b259ad26482a6e5e1c10d7da7235
402202927 <218518161+402202927@users.noreply.github.com>
2025-11-10 22:42:59 +0200

    Update models.py

--- mapper/models.py
+++ mapper/models.py
@@ -1,1 +1,29 @@
-# Models placeholder
+# Models
+
+from django.db import models
+
+class BusStop(models.Model):
+    stop_id = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=200)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+class BusRoute(models.Model):
+    route_id = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=200)
+    stops = models.ManyToManyField(BusStop, related_name='routes')
+
+    def __str__(self):
+        return self.name
+
+class Trip(models.Model):
+    trip_id = models.CharField(max_length=50, unique=True)
+    route = models.ForeignKey(BusRoute, on_delete=models.CASCADE, related_name='trips')
+    departure_time = models.TimeField()
+    arrival_time = models.TimeField()
+
+    def __str__(self):
+        return f"{self.route.name} - {self.trip_id}"
