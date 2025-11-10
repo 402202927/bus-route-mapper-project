fafb4687bd2d0db74afb274b15ac7a339a87d448
402202927 <218518161+402202927@users.noreply.github.com>
2025-11-10 22:41:30 +0200

    Update map.js

--- mapper/static/mapper/js/map.js
+++ mapper/static/mapper/js/map.js
@@ -1,1 +1,4 @@
-// JS placeholder
+var map = L.map('map').setView([-33.9249, 18.4241], 12);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
