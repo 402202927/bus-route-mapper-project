4475199d57a06ad52e413317bb28779bc206edd6
402202927 <218518161+402202927@users.noreply.github.com>
2025-11-10 22:43:33 +0200

    Update serializers.py

--- mapper/serializers.py
+++ mapper/serializers.py
@@ -1,1 +1,17 @@
-# Serializers placeholder
+from rest_framework import serializers
+from .models import BusStop, BusRoute, Trip
+
+class BusStopSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BusStop
+        fields = '__all__'
+
+class BusRouteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BusRoute
+        fields = '__all__'
+
+class TripSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Trip
+        fields = '__all__'
