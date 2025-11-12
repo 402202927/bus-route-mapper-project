a7eddd3cae2d04e0198b6e9f6de945eaaa169393
402202927 <218518161+402202927@users.noreply.github.com>
2025-11-12 13:38:41 +0200

    Update map.js

--- mapper/static/mapper/js/map.js
+++ mapper/static/mapper/js/map.js
@@ -2,3 +2,9 @@ var map = L.map('map').setView([-33.9249, 18.4241], 12);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
+
+L.icon({
+    iconUrl: '/static/mapper/img/icons/bus-stop.svg',
+    iconSize: [32, 32]  // still define size for consistency
+});
+
